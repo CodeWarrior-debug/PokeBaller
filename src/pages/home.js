9f9f7a03cebc61449ca0a3778d1eeb9125c0231b
CodeWarrior-debug <jpjordan1987@gmail.com>
2022-12-13 23:59:37 -0500

    now adding more to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,23 @@ import React from "react";
 import LoginHeader from "../components/loginheader";
 import Navbar from "../components/navbar";
 
-
 const Home = () => {
   return (
     <>
       <LoginHeader />
-    <Navbar/>
+      <Navbar />
+      <div className="container-fluid justify-content-center text-center container mt-3">
+        <h2>
+          {" "}
+          <span className="poke-red">Welcome to Poke</span>Baller!
+        </h2>
+        <p>
+          Use PokeBaller to add and keep track of cards in your own personal
+          digital collection.
+        </p>
+        <p>Navigate to "add" to begin compiling cards to your collection.</p>
+        <p>To see your collection, navigate to "view."</p>
+      </div>
     </>
   );
 };
